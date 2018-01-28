799db53046a295fa23fcd0ab4f2cb769fcad9a1d
Javier Machin <javier_machin_acosta@hotmail.com>
2018-01-28 14:26:25 +0000

    Add aditional functionality and prevent invalid operations

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 
 let calculatorPad = document.getElementById("calculatorPad");
 let diplayNumbers = document.getElementById("displayNumbers");
-let isOperation = RegExp("[^0-9^,]");
+let isOperation = RegExp("[^0-9^.]");
 let number1;
 let number2;
 let currentOperation = "";
@@ -25,7 +25,30 @@ function divideNumbers(value1, value2) {
 }
 
 function Operate(operation, value1, value2) {
+	console.log(operation);
+	console.log(value1);
+	console.log(value2);
 	return operation(value1, value2);
+}
+
+function selectOperation(string1, operationPositions) {
+	let operation;
+	let operationCharacter = string1.charAt(operationPositions[0]);
+	switch (operationCharacter) {
+		case "+":
+		operation = addNumbers;
+		break;
+		case "-":
+		operation = subtractNumbers;
+		break;
+		case "x":
+		operation = multiplyNumbers;
+		break;
+		case "÷":
+		operation = divideNumbers;
+		break;
+	}
+	return operation;
 }
 
 function checkStatus(string1) {
@@ -33,16 +56,28 @@ function checkStatus(string1) {
 	let operationPositions = [];
 	for (let i = 0; i < string1.length; i++) {
 		let currentChar = string1.charAt(i);
-		if (isOperation.test(currentChar)) {
+		if (isOperation.test(currentChar) && i != 0) {
 			numberOperations++;
-			operationPositions.push(i);
+			if (operationPositions.length < 2) {
+				operationPositions.push(i);
+			} else {
+				operationPositions[1] = i;
+			}
 		}
 	}
 	if (numberOperations >= 2) {
 		number1 = Number(string1.slice(0, operationPositions[0]));
 		number2 = Number(string1.slice((operationPositions[0] + 1), operationPositions[1]));
-		console.log(number1 + " " + number2);
-		// we have the 2 numbers, now create function to pick operation
+		if (!number1) number1 = 0;
+		if (!number2) number2 = 0;
+		let operation = selectOperation(string1, operationPositions);
+		displayNumbers.innerHTML = Operate(operation, number1, number2);
+		number1 = Operate(operation, number1, number2);
+		number2 = undefined;
+		operationPositions[0] = operationPositions[1];
+		accumulated = number1 + string1.charAt(operationPositions[1]);
+		// division 0 breaks it
+		// what happens when equal is clicked
 	}
 }
 
