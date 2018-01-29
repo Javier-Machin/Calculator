6f2703ad4c2550e88f2026364512bde50e2b386e
Javier Machin <javier_machin_acosta@hotmail.com>
2018-01-29 14:36:39 +0000

    Add code to handle more application-crashing operations

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,11 @@ let diplayNumbers = document.getElementById("displayNumbers");
 let isOperation = RegExp("[^0-9^.]");
 let number1;
 let number2;
-let currentOperation = "";
 let accumulated ="";
 let operationPositions = [];
+let numberOperations = 0;
+let operatorCount = 0;
+
 
 function addNumbers(value1, value2) {
 	return value1 + value2;
@@ -25,15 +27,12 @@ function divideNumbers(value1, value2) {
 }
 
 function Operate(operation, value1, value2) {
-	console.log(operation);
-	console.log(value1);
-	console.log(value2);
 	return operation(value1, value2);
 }
 
-function selectOperation(string1, operationPositions) {
+function selectOperation(operationPositions) {
 	let operation;
-	let operationCharacter = string1.charAt(operationPositions[0]);
+	let operationCharacter = accumulated.charAt(operationPositions[0]);
 	switch (operationCharacter) {
 		case "+":
 		operation = addNumbers;
@@ -47,44 +46,90 @@ function selectOperation(string1, operationPositions) {
 		case "÷":
 		operation = divideNumbers;
 		break;
+		case "=":
+		operation = resultNumbers;
+		break;
 	}
 	return operation;
 }
 
-function checkStatus(string1) {
-	let numberOperations = 0;
+function clearAll() {
+	number1 = undefined;
+	number2 = undefined;
+	accumulated = "";
+	operationPositions = [];
+	displayNumbers.innerHTML = 0;
+}
+
+function removeRepeatedOps(button) {
+	if (isOperation.test(accumulated.charAt(accumulated.length - 1)) && isOperation.test(button)) {
+		accumulated = accumulated.slice(0, accumulated.length - 1) + button;
+	} else  if (button == "=" && numberOperations == 0) { 
+		return;
+	} else {
+	accumulated += button;
+	}
+}
+
+function checkStatus(button) {
+	operatorCount = 0;
+	numberOperations = 0;
 	let operationPositions = [];
-	for (let i = 0; i < string1.length; i++) {
-		let currentChar = string1.charAt(i);
-		if (isOperation.test(currentChar) && i != 0) {
-			numberOperations++;
-			if (operationPositions.length < 2) {
-				operationPositions.push(i);
+	let previousChar;
+	for (let i = 0; i < accumulated; i++) {
+		if (isOperation.test(accumulated.charAt(i))) {
+			operatorCount++;
+		}
+	}
+	if (accumulated.includes("=") && operatorCount <= 1) {
+		accumulated.replace("=", "");
+	}
+	displayNumbers.innerHTML = accumulated;
+	for (let i = 0; i < accumulated.length; i++) {
+		let currentChar = accumulated.charAt(i);
+		if (isOperation.test(currentChar) && i != 0 && (!isOperation.test(previousChar))) {
+			if (currentChar != "=") {
+				numberOperations++;
+			}
+			if (!(operationPositions[0]) && currentChar != "=") {
+				operationPositions[0] = i;
 			} else {
 				operationPositions[1] = i;
 			}
 		}
+		previousChar = currentChar;
 	}
-	if (numberOperations >= 2) {
-		number1 = Number(string1.slice(0, operationPositions[0]));
-		number2 = Number(string1.slice((operationPositions[0] + 1), operationPositions[1]));
+	if (numberOperations >= 2|| accumulated.charAt(operationPositions[1]) == "=" && numberOperations > 0) {
+		number1 = Number(accumulated.slice(0, operationPositions[0]));
+		number2 = Number(accumulated.slice((operationPositions[0] + 1), operationPositions[1]));
 		if (!number1) number1 = 0;
 		if (!number2) number2 = 0;
-		let operation = selectOperation(string1, operationPositions);
+		let operation = selectOperation(operationPositions);
+		if (operation == divideNumbers && number2 == 0) {
+			alert("One does not simply divide by zero");
+			clearAll();
+			return;
+		}
 		displayNumbers.innerHTML = Operate(operation, number1, number2);
-		number1 = Operate(operation, number1, number2);
-		number2 = undefined;
-		operationPositions[0] = operationPositions[1];
-		accumulated = number1 + string1.charAt(operationPositions[1]);
-		// division 0 breaks it
-		// what happens when equal is clicked
+		number1 = Number(displayNumbers.innerHTML);
+		if (accumulated.charAt(operationPositions[1]) != "=") {
+			operationPositions[0] = operationPositions[1];
+			accumulated = String(number1 + accumulated.charAt(operationPositions[0]));
+		} else {
+			accumulated = String(number1);
+		}
+		if (accumulated.charAt(accumulated.length - 1) == "=") {
+			accumulated = accumulated.slice(0, accumulated.length);
+		}
+		// After clicking equal it allows the equal sign to be included in accumulated while it should not.
 	}
 }
 
 function buttonPressed(e) {
 	let button = e.target.innerHTML;
-	accumulated += button;
-	checkStatus(accumulated);
+	removeRepeatedOps(button);
+	checkStatus(accumulated, button);
+
 }
 
 calculatorPad.addEventListener("click", buttonPressed);
